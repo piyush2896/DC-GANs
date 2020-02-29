bbbbc87115b74d46b55e9d326859f194fad419f2
piyush2896 <piyushmalhotra28@gmail.com>
2020-02-29 14:29:57 -0700

    Add generator

---
+++ models/generator.py
@@ -0,0 +1,35 @@
+from torch import nn
+
+class ConvTranspose2dBNReLu(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernerl_size,
+                 strides,
+                 padding,
+                 bias=False):
+        super(ConvTranspose2dBNReLu, self).__init__()
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels, out_channels, kernerl_size,
+                strides, padding, bias=bias),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, X):
+        return self.main(X)
+
+class Generator(nn.Module):
+    def __init__(self, in_dim, filters, out_channels):
+        super(Generator, self).__init__()
+        self.generate = nn.Sequential(
+            ConvTranspose2dBNReLu(in_dim, filters*8, 4, 1, 0),
+            ConvTranspose2dBNReLu(filters*8, filters*4, 4, 2, 1),
+            ConvTranspose2dBNReLu(filters*4, filters*2, 4, 2, 1),
+            ConvTranspose2dBNReLu(filters*2, filters, 4, 2, 1),
+            ConvTranspose2dBNReLu(filters, out_channels, 4, 2, 1)
+        )
+
+    def forward(self, X):
+        return self.generate(X)
