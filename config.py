0a168bda56a8d575616c993287a24c76d6ce02b8
piyush2896 <piyushmalhotra28@gmail.com>
2020-02-29 21:34:21 -0700

    Add Configurations

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+    BATCH_SIZE = 128
+    IMG_SIZE = 64
+    N_CHANNELS = 3
+    N_DIMS = 100
+    GEN_FEATURE_MAPS = 64
+    DIS_FEATURE_MAPS = 64
+    NUM_EPOCHS = 50
+    LEARNING_RATE = 2e-4
+    BETA = 0.5
+    WORKERS = 2
+    ROOT = './data/celeba'
