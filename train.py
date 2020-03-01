7299c6059f73aabaffb660fd74bd1c7f1f2e65d7
piyush2896 <piyushmalhotra28@gmail.com>
2020-03-01 00:52:19 -0700

    Add training loop

--- train.py
+++ train.py
@@ -1,13 +1,16 @@
+import os
+
 import torch
 from torch import nn
 from torch import optim
+import dataloader
 
 import models
 
 class Trainer:
     def __init__(self, Config):
-        self._init_generator()
-        self._init_discriminator()
+        self.__init_generator()
+        self.__init_discriminator()
         self.fixed_noise = torch.randn(
             64, Config.N_DIMS, 1, 1, device=Config.DEVICE)
 
@@ -36,21 +39,20 @@ class Trainer:
         }
         self.Config = Config
 
-    def _init_generator(self, Config):
+    def __init_generator(self, Config):
         self.netG = models.Generator(
             Config.N_DIMS, Config.GEN_FEATURE_MAPS, Config.N_CHANNELS)
         self.netG.to(Config.DEVICE)
         self.netG.apply(models.weights_init)
 
-    def _init_discriminator(self, Config):
+    def __init_discriminator(self, Config):
         self.netD = models.Discriminator(
             Config.N_CHANNELS, Config.DIS_FEATURE_MAPS)
         self.netD.to(Config.DEVICE)
         self.netD.apply(models.weights_init)
 
-    def train_on_real(self, real_imgs):
+    def __train_on_real(self, real_imgs):
         self.netD.zero_grad()
-        real_imgs = real_imgs.to(self.Config.DEVICE)
         label = torch.full(
             (real_imgs.size(0),), self.real_label, device=self.Config.DEVICE)
         out = self.netD(real_imgs).view(-1)
@@ -59,7 +61,7 @@ class Trainer:
         self.current_iter['D_x'] = out.mean().item()
         self.current_iter['err_real_imgs'] = err_real_imgs.item()
 
-    def train_on_fake(self):
+    def __train_on_fake(self):
         noise = torch.randn(
             self.Config.BATCH_SIZE, self.Config.N_DIMS,
             1, 1, device=self.Config.DEVICE)
@@ -95,4 +97,37 @@ class Trainer:
             self.current_iter['D_G_z1']))
 
     def train(self):
-        pass
+        self.img_list = []
+        self.losses = {
+            'G': [],
+            'D': []
+        }
+        it_ctr = 0
+
+        data = dataloader.CelebFacesDataloaders(
+            self.Config.DATA_ROOT, self.Config.IMG_SIZE,
+            self.Config.BATCH_SIZE, self.Config.NUM_WORKERS)
+
+        print('Starting the training...')
+        for epoch in range(self.Config.EPOCHS):
+            for i, data in enumerate(data, 0):
+
+                real_imgs, _ = data
+                real_imgs = real_imgs.to(self.Config.DEVICE)
+                self.__train_on_real(real_imgs)
+                self.__train_on_fake()
+
+                if it_ctr % self.Config.LOG_STEP == 0:
+                    self.__print_iteration(epoch, i, len(data))
+
+                if ((it_ctr % self.Config.IMG_LOG_STEP == 0) or
+                    ((epoch == self.Config.EPOCHS - 1) and (i == len(data)-1))):
+                    with torch.no_grad():
+                        fake = self.netG(self.fixed_noise).detach().cpu()
+                    self.img_list.append(fake)
+                    torch.save(self.netG.state_dict,
+                        os.path.join(self.Config.model_path, 'generator.model'))
+                    torch.save(self.netD.state_dict,
+                        os.path.join(self.Config.model_path, 'discriminator.model'))
+
+                it_ctr += 1
